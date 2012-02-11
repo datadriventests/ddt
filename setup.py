8fae95add05caec02bde2fb9e70735ef5dd2c34d
Carles Barrobés <carles@barrobes.com>
2012-02-11 23:56:25 +0000

    Add setup script

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# # coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='ddt',
+    description='Data-Driven/Decorated Tests',
+    long_description='A library to multiply test cases',
+    version='0.1.1',
+    author='Carles Barrobés',
+    author_email='carles@barrobes.com',
+    url='https://github.com/txels/ddt',
+    py_modules=['ddt'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+    ],
+)
+
