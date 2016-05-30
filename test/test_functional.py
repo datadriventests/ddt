9abb569d2ecd760078bc4038d52a7dd532aecd59
Pradyun S. Gedam <pradyunsg@gmail.com>
2016-05-30 13:36:14 +0530

    Fix the no-YAML installed test

--- test/test_functional.py
+++ test/test_functional.py
@@ -307,6 +307,16 @@ def test_load_yaml_without_yaml_support():
     Test that YAML files are not loaded if YAML is not installed.
     """
 
-    @file_data('test_data_dict.yaml')
-    def test_file_data_yaml_dict(value):
-        assert_true(has_three_elements(value))
+    @ddt
+    class NoYAMLInstalledTest(object):
+
+        @file_data('test_data_dict.yaml')
+        def test_file_data_yaml_dict(self, value):
+            assert_true(has_three_elements(value))
+
+    tests = filter(_is_test, NoYAMLInstalledTest.__dict__)
+
+    obj = NoYAMLInstalledTest()
+    for test in tests:
+        method = getattr(obj, test)
+        assert_raises(ValueError, method)
