ea4f205fac046ca45caac3d15d4c1b981eb5ca48
Jake Lishman <jake.lishman@ibm.com>
2021-09-27 22:56:39 +0100

    Make index_len argument to idata optional
    PR #92 added an extra argument to the `idata` decorator.  The new
    `index_len` argument had no default value, and so the calling convention
    was incompatible with older releases of `ddt`.  This commit makes the
    second argument optional, with the previous default value inferred if it
    is not supplied.  The single-argument form of `idata` will now work
    again, while the newer method allowing it to be overridden is still
    supported.

--- ddt.py
+++ ddt.py
@@ -90,20 +90,30 @@ def data(*values):
     Should be added to methods of instances of ``unittest.TestCase``.
 
     """
-    return idata(values, len(str(len(values))))
+    return idata(values)
 
 
-def idata(iterable, index_len):
+def idata(iterable, index_len=None):
     """
     Method decorator to add to your test methods.
 
     Should be added to methods of instances of ``unittest.TestCase``.
 
+    :param iterable: iterable of the values to provide to the test function.
+    :param index_len: an optional integer specifying the width to zero-pad the
+        test identifier indices to.  If not provided, this will add the fewest
+        zeros necessary to make all identifiers the same length.
     """
+    if index_len is None:
+        # Avoid consuming a one-time-use generator.
+        iterable = tuple(iterable)
+        index_len = len(str(len(iterable)))
+
     def wrapper(func):
         setattr(func, DATA_ATTR, iterable)
         setattr(func, INDEX_LEN, index_len)
         return func
+
     return wrapper
 
 
